6455d6be9ab1bf462abefb25008d0cf8b9e47394
Henrique-Ambiel <146786119+Henrique-Ambiel@users.noreply.github.com>
2026-05-04 16:06:35 -0300

    Adicionando inimigo e começando sistema de dano

---
+++ Assets/Scripts/Player/Health.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private int lives;
+
+    public event Action OnDead;
+    public event Action OnHurt;
+
+    public void TakeDamage()
+    {
+        lives--;
+        HandleDamageTaken();
+
+    }
+
+    private void HandleDamageTaken()
+    {
+        if (lives <= 0)
+        {
+            OnDead?.Invoke();
+        }
+        else
+        {
+            OnHurt?.Invoke();
+        }
+    }
+}
